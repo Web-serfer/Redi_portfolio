573423450241f1ee2c5ae4ef4ac1c626f881b440
RediBedi <redibedi2019@gmail.com>
2023-03-15 21:15:04 +0500

    15/03/23

---
+++ src/components/about/dataAbout.js
@@ -0,0 +1,35 @@
+const dataArticles = [
+  {
+    id: 1,
+    classCard: "about__card",    
+    pack: "dashicons",
+    name: "awards",
+    size: 28,
+    classIcon: "about__icon",
+    h4: "Опыт",  
+    small: "3 года разработки",  
+  },
+  {
+    id: 2,
+    classCard: "about__card",    
+    pack: "la",
+    name: "users",
+    size: 28,
+    classIcon: "about__icon",  
+    h4: "Клиенты", 
+    small: "200+ клиентов",  
+  },
+  {
+    id: 3,
+    classCard: "about__card",    
+    pack: "file-icons",
+    name: "microsoft-project",
+    size: 28,
+    classIcon: "about__icon",
+    h4: "Проекты",
+    small: "80+ завершенных",      
+  }  
+  
+];
+
+export default dataArticles;
